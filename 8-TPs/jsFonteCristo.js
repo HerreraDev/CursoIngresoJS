7ca8ed402e428245f6bfaeb7b990c80a5ea3aad0
alumno <alumno@utnfra.com.ar>
2020-02-28 21:57:49 -0300

    tp montecristo

--- 8-TPs/jsFonteCristo.js
+++ 8-TPs/jsFonteCristo.js
@@ -9,5 +9,44 @@ e.	Se pedirán un número positivo y se mostrará la cantidad de números Primos
 */
 function ComenzarIngreso () 
 {
- 	
+     var numero = document.getElementById("numero").value;
+}
+function NumerosPares()
+{
+    var numero = document.getElementById("numero").value;
+    var contadorPares = 0;
+    for(var i = numero; i >= 0; i--)
+    {   
+        if(i % 2 == 0)
+        {
+            contadorPares ++;
+        }
+    }
+    alert("Cantidad de pares: " + contadorPares);
+}
+function NumerosImpares()
+{
+    var numero = document.getElementById("numero").value;
+    var contadorImpares = 0;
+    for(var i = numero; i >= 0; i--)
+    {   
+        if(i % 2 != 0)
+        {
+            contadorImpares++;
+        }
+    }
+    alert("Cantidad de pares: " + contadorImpares);
+}
+function NumerosDivisibles()
+{
+    var numero = document.getElementById("numero").value;
+    var contadorDiv = 0;
+    for(var i = 100; i>=0; i--)
+    {
+        if(i % numero == 0)
+        {
+            contadorDiv++;
+        }
+    }
+    alert("Cantidad de divisibles por dicho numero: " + contadorDiv);
 }
